794834df61c434ffa0681684f329650641abd544
Cantika Dewi Maharani <66821310+cantika28@users.noreply.github.com>
2021-04-01 09:12:41 +0700

    Update index.php

--- Minggu8.2/ci/application/views/index.php
+++ Minggu8.2/ci/application/views/index.php
@@ -256,7 +256,7 @@
                     </div>
                     <div class="col-lg-4">
                         <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/img/team/2.jpg')?>" alt="" />
+                            <img class="mx-auto rounded-circle" src="<?php echo base_url('assets/assets/img/team/2.jpg')?>" alt="" />
                             <h4>Larry Parker</h4>
                             <p class="text-muted">Lead Marketer</p>
                             <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
